54b46b2fcf45af2f97bea5cf28f10112c0dea2f8
Mateus Santos <m.santos.azeve@gmail.com>
2021-06-21 15:51:36 -0300

    Tela de Login

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,21 +1,40 @@
-import React, { useState } from "react";
-import { View, Text, TextInput } from "react-native";
+import React from "react";
+import { View, Text, Image, StatusBar } from "react-native";
 
+import { ButtonIcon } from "../../components/buttonIcon";
+
+import IllustrationImg from "../../assets/illustration.png";
 import { styles } from "./styles";
 
 export function SignIn() {
-  const [text, setText] = useState("");
-
   return (
     <View style={styles.container}>
-      <Text>Hello World, NLW Together</Text>
-
-      <TextInput
-        style={styles.input}
-        onChangeText={(value) => setText(value)}
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
       />
 
-      <Text>Você digitou: {text}</Text>
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize{"\n"}
+          suas jogatinas{"\n"}
+          facilmente
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games{"\n"}
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon />
+      </View>
     </View>
   );
 }

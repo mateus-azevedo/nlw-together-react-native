0b7398c0e76cec43d8424ac377ddfdf7fe876814
Mateus Santos <m.santos.azeve@gmail.com>
2021-08-06 17:00:31 -0300

    feat: uso do context para aplicacao
    simplificando o uso dos contextos deixando disponivel para toda aplicação

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { View, Text, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
+import { useAuth } from "../../hooks/auth";
+
 import { ButtonIcon } from "../../components/ButtonIcon";
 import { Background } from "../../components/Background";
 
@@ -10,6 +12,9 @@ import { styles } from "./styles";
 
 export function SignIn() {
   const navigation = useNavigation();
+
+  const { user } = useAuth();
+  console.log(user);
 
   function handleSignIn() {
     navigation.navigate("Home");

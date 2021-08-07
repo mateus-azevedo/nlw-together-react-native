d76a709c43db4064083c437041cd2eb5ae329edf
Mateus Santos <m.santos.azeve@gmail.com>
2021-08-07 02:19:44 -0300

    feat: finalizando autenticacao com OAuth2 e definindo rotas publicas e privadas

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -21,11 +21,18 @@ type User = {
 
 type AuthContextData = {
   user: User;
+  loading: boolean;
   signIn: () => Promise<void>;
 };
 
 type AuthProviderProps = {
   children: ReactNode;
+};
+
+type AuthorizationResponse = AuthSession.AuthSessionResult & {
+  params: {
+    access_token: string;
+  };
 };
 
 export const AuthContext = createContext({} as AuthContextData);
@@ -39,9 +46,27 @@ function AuthProvider({ children }: AuthProviderProps) {
       setLoading(true);
 
       const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
-      const response = await AuthSession.startAsync({ authUrl });
+      const { type, params } = (await AuthSession.startAsync({
+        authUrl,
+      })) as AuthorizationResponse;
 
-      console.log(response);
+      if (type === "success") {
+        api.defaults.headers.authorization = `Bearer ${params.access_token}`;
+
+        const userInfo = await api.get("/users/@me");
+        const firstName = userInfo.data.username.split(" ")[0];
+
+        userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`;
+
+        setUser({
+          ...userInfo.data,
+          firstName,
+          token: params.access_token,
+        });
+        setLoading(false);
+      } else {
+        setLoading(false);
+      }
     } catch {
       // Lanço o erro para quem chamou
       throw new Error("Não foi possivel autenticar");
@@ -49,7 +74,7 @@ function AuthProvider({ children }: AuthProviderProps) {
   }
 
   return (
-    <AuthContext.Provider value={{ user, signIn }}>
+    <AuthContext.Provider value={{ user, loading, signIn }}>
       {children}
     </AuthContext.Provider>
   );
